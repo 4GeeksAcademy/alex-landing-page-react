da5ef9d59de0592f459bd65c44d7b798fbedf425
Alexander Morales <alexandermorales.n1@gmail.com>
2025-04-01 21:05:50 +0000

    add props to cards

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
 return (
     <div className="col-12 col-md-3 card g-3" >
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmStm5d-komRukWTSOYWnVAhDo5i2PbrBhIA&s" className="card-img-top" alt="..."/>
         <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+            <h5 className="card-title">{props.title}</h5>
+            <p className="card-text">{props.text}</p>
+            <a href="#" className="btn btn-primary">{props.label}</a>
         </div>
     </div>
 )
--- src/js/components/CardsSection.jsx
+++ src/js/components/CardsSection.jsx
@@ -4,10 +4,10 @@ import Card from './Card'
 const CardsSection = () => {
     return (
         <div className='row d-flex justify-end p-3 '>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
+            <Card title="Card Title" text="Some quick example text to build on the card title and make up the bulk of the card's content." label="Go somewhere"/>
+            <Card title="Card Title" text="Some quick example text to build on the card title and make up the bulk of the card's content." label="Go somewhere"/>
+            <Card title="Card Title" text="Some quick example text to build on the card title and make up the bulk of the card's content." label="Go somewhere"/>
+            <Card title="Card Title" text="Some quick example text to build on the card title and make up the bulk of the card's content." label="Go somewhere"/>
         </div>
     )
 }
